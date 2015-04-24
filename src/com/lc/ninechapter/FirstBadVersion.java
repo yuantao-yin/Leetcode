413cff9478bc70e8a81e43ddf40cf2e30461d6a7
Yuantao <yin0823@gmail.com>
2015-04-24 17:37:30 -0400

    Create FirstBadVersion.java

---
+++ src/com/lc/ninechapter/FirstBadVersion.java
@@ -0,0 +1,26 @@
+/**
+ * public class VersionControl {
+ *     public static boolean isBadVersion(int k);
+ * }
+ * you can use VersionControl.isBadVersion(k) to judge wether 
+ * the kth code version is bad or not.
+*/
+class FirstBadVersion {
+    /**
+     * @param n: An integers.
+     * @return: An integer which is the first bad version.
+     */
+    public int findFirstBadVersion(int n) {
+        int start = 1, end = n;
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            if (VersionControl.isBadVersion(mid)) {
+                end = mid - 1;
+            } else {
+                start = mid + 1;
+            }
+        }
+        return start;
+    }
+}
+
