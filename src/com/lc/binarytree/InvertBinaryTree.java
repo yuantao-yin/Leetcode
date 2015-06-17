98e946e24cb8f0b47c0135aab6d67631c9798cef
Yuantao <yin0823@gmail.com>
2015-06-17 13:24:08 -0400

    Create InvertBinaryTree.java

---
+++ src/com/lc/binarytree/InvertBinaryTree.java
@@ -0,0 +1,34 @@
+/**
+ * Definition of TreeNode:
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left, right;
+ *     public TreeNode(int val) {
+ *         this.val = val;
+ *         this.left = this.right = null;
+ *     }
+ * }
+ */
+public class Solution {
+    /**
+     * @param root: a TreeNode, the root of the binary tree
+     * @return: nothing
+     */
+    public void invertBinaryTree(TreeNode root) {
+        if (root == null) {
+            return;
+        }
+        TreeNode temp = root.left;
+        root.left = root.right;
+        root.right = temp;
+        
+        if (root.left != null) {
+            invertBinaryTree(root.left);
+        }
+        if (root.right != null) {
+            invertBinaryTree(root.right);
+        }
+        
+    }
+
+}
