faba43258686cbb090c1a6434cf0d0db865e73da
Yuantao <yin0823@gmail.com>
2015-06-23 23:50:41 -0400

    Create LongestCommonPrefix.java

---
+++ src/com/lc/string/LongestCommonPrefix.java
@@ -0,0 +1,24 @@
+public class Solution {
+    /**
+     * @param strs: A list of strings
+     * @return: The longest common prefix
+     */
+    public String longestCommonPrefix(String[] strs) {
+        if (strs == null || strs.length == 0) {
+            return "";
+        }
+        // use first one as prefix, compare to other string in the list, until end.
+        String prefix = strs[0];
+        for (int i = 1; i < strs.length; i++) {
+            int j = 0;
+            while (j < strs[i].length() && j < prefix.length() && prefix.charAt(j) == strs[i].charAt(j)) {
+                j++;
+            }
+            if (j == 0) {
+                return "";
+            }
+            prefix = prefix.substring(0,j);
+        }
+        return prefix;
+    }
+}
