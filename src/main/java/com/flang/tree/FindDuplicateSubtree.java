4d7a42f179a7e0df0842ad9ff9caa64dc8821704
yuantao-yin <yin0823@gmail.com>
2020-09-11 02:00:24 -0400

    tree pratice

---
+++ src/main/java/com/flang/tree/FindDuplicateSubtree.java
@@ -0,0 +1,68 @@
+package main.java.com.flang.tree;
+
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/*
+652. Given the root of a binary tree, return all duplicate subtrees.
+For each kind of duplicate subtrees, you only need to return the root node of any one of them.
+Two trees are duplicate if they have the same structure with the same node values.
+ */
+public class FindDuplicateSubtree {
+
+    Map<String, Integer> map;
+    List<TreeNode> list;
+
+    public List<TreeNode> findDuplicateSubtrees(TreeNode root) {
+        // use map to store each 3 nodes as string and count the occurrence
+        map = new HashMap();
+        // for every 2 string that is same, save to the list
+        list = new ArrayList<>();
+        duplicate(root);
+        return list;
+    }
+
+    private String duplicate(TreeNode node) {
+        // base case
+        if (node == null) {
+            return "X";
+        }
+        String l = duplicate(node.left);
+        String r = duplicate(node.right);
+        String s = node.val + "," + l + "," + r; // 1,X,X
+        map.put(s, map.getOrDefault(s, 0) + 1);
+        if (map.get(s) == 2) {
+            list.add(node);
+        }
+        return s;
+    }
+
+    class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+}
+
+
+
+
+
+
---
+++ src/main/java/com/flang/tree/SubtreeOfAnotherTree.java
@@ -0,0 +1,52 @@
+package main.java.com.flang.tree;
+
+/*
+572. Given two non-empty binary trees s and t, check whether tree t has exactly the same structure and
+node values with a subtree of s. A subtree of s is a tree consists of a node in s and all
+of this node's descendants. The tree s could also be considered as a subtree of itself.
+*/
+
+
+public class SubtreeOfAnotherTree {
+
+    class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public boolean isSubtree(TreeNode s, TreeNode t) {
+        if (s == null) {
+            return false;
+        } else if (isSameTree(s, t)) {
+            return true;
+        } else {
+            return isSubtree(s.left, t) || isSubtree(s.right, t);
+        }
+    }
+
+    public boolean isSameTree(TreeNode s, TreeNode t) {
+        if (s == null || t == null) {
+            return s == null && t == null;
+        } else if (s.val == t.val) {
+            return isSameTree(s.left, t.left) && isSameTree(s.right, t.right);
+        } else {
+            return false;
+        }
+    }
+
+
+}
