4dd9108ad193c0142eb6ab523c957342cefffb28
yuantao-yin <yin0823@gmail.com>
2020-09-29 01:12:58 -0400

    Update CloneBinaryTreeWithRandomPointer.java

--- src/main/java/com/flang/clone/CloneBinaryTreeWithRandomPointer.java
+++ src/main/java/com/flang/clone/CloneBinaryTreeWithRandomPointer.java
@@ -6,21 +6,25 @@ import java.util.Map;
 public class CloneBinaryTreeWithRandomPointer {
 
     Map<TreeNode, TreeNode> map=new HashMap<>();
-    
+
     public TreeNode cloneTree(TreeNode root) {
         return deepCopy(root);
     }
 
     private TreeNode deepCopy(TreeNode root) {
         if (root == null) return null;
+        // if the node is already being copied, return the copied node
         if (map.get(root) != null) {
             return map.get(root);
         }
         TreeNode copy = new TreeNode(root.data);
         map.put(root, copy);
+        // dfs left node
         copy.left = deepCopy(root.left);
+        // dfs right node
         copy.right = deepCopy(root.right);
         copy.random = deepCopy(root.random);
+        // return copy node
         return copy;
     }
 }
