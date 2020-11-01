951f51679afee532cc0b349885a28418211490cb
YuantaoYin <yuantao.yin@delta.com>
2020-10-31 20:55:55 -0400

    Create LowestCommonAncestor.java

---
+++ src/main/java/com/msft/LowestCommonAncestor.java
@@ -0,0 +1,34 @@
+package com.msft;
+
+public class LowestCommonAncestor {
+
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        while (root != null) {
+            if (p.val < root.val && q.val > root.val) {
+                return root;
+            } else if (p.val < root.val && q.val < root.val) {
+                root = root.left;
+            } else if (p.val > root.val && q.val > root.val) {
+                root = root.right;
+            } else {
+                return root;
+            }
+        }
+        return root;
+    }
+
+    public static void main(String[] args) {
+
+    }
+
+    class TreeNode {
+        public int val;
+        public TreeNode left, right;
+
+        public TreeNode(int val) {
+            this.val = val;
+            this.left = this.right = null;
+        }
+    }
+
+}
