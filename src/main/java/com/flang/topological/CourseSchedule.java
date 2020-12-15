07463af9ed469192581ff783c4c26578cb2ee2a2
YuantaoYin <yuantao.yin@delta.com>
2020-12-14 19:03:12 -0500

    comments

--- src/main/java/com/flang/topological/CourseSchedule.java
+++ src/main/java/com/flang/topological/CourseSchedule.java
@@ -27,10 +27,12 @@ public class CourseSchedule {
                 queue.offer(i);
             }
         }
+        int visited = 0;
         while (!queue.isEmpty()) {
             int cur = queue.remove();
+            visited++;
             for (int toTake : graph[cur]) {
-                /* tart looping each neighbor of current course, decrease inDegree count by 1,
+                /* start looping each neighbor of current course, decrease inDegree count by 1,
                    if inDegree count is 0, we know current course has no prerequiste course anymore,
                    (no more neighbor for current course),so we can add it to the queue
                 */
@@ -40,17 +42,14 @@ public class CourseSchedule {
                 }
             }
         }
-        for (int degree : inDegree) {
-            if (degree != 0) {
-                return false;
-            }
-        }
 
-        return true;
+        return visited == numCourses;
     }
 
     public static void main(String[] args) {
         int[][] courseConn = {{1,2},{0,3},{1,4},{3,1}};
-        System.out.println(CourseSchedule.canFinish(4, courseConn));
+        int[][] courseConn2 = {{1,0}};
+        System.out.println(CourseSchedule.canFinish(5, courseConn));
+        System.out.println(CourseSchedule.canFinish(2, courseConn2));
     }
 }
--- src/main/java/com/flang/topological/TopologicalSort.java
+++ src/main/java/com/flang/topological/TopologicalSort.java
@@ -43,11 +43,21 @@ public class TopologicalSort {
 
         }
 
+        /**
+         * This method takes the vertex v and makes a recursive call to its unvisited neighboring vertices.
+         * It chooses from one of the children nodes and then moves down and mark the children node of that vertex.
+         * If it finds that a certain vertex has no neighbors or unvisited neighbors, it pushes this vertex into the
+         * stack and traverses back to the last vertex. This process checks if the vertex it has other unvisited
+         * children nodes. The same cycle then continues once all the nodes have been checked.
+         * @param v
+         * @param visited
+         * @param stack
+         */
         private void helper(int v, boolean[] visited, Stack stack) {
             visited[v] = true;
             Integer i;
             // loop all the vertices adjacent to this vertex
-            Iterator<Integer> it = adj[v].iterator();
+            Iterator<Integer> it = adj[v].listIterator();
             while (it.hasNext()) {
                 i = it.next();
                 if (!visited[i]) {
@@ -72,6 +82,10 @@ public class TopologicalSort {
         System.out.println("Following is a Topological " +
                 "sort of the given graph");
         g.topologicalSort();
+
+        int [][] test = new int[][]{{1,2},{2,3},{4,5}};
+        System.out.println(test.length);
+        System.out.println(test[0].length);
     }
 
 
