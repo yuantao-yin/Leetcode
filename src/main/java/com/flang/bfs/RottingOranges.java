ce6161d4201a2784c44f5ea107ba3bfecdb9cae8
YuantaoYin <yuantao.yin@delta.com>
2021-01-11 01:44:06 -0500

    bfs

--- src/main/java/com/flang/bfs/RottingOranges.java
+++ src/main/java/com/flang/bfs/RottingOranges.java
@@ -8,6 +8,7 @@ import java.util.Queue;
 import java.util.Set;
 
 /*
+LC 994
 In a given grid, each cell can have one of three values:
 
 the value 0 representing an empty cell;
@@ -16,7 +17,7 @@ the value 2 representing a rotten orange.
 Every minute, any fresh orange that is adjacent (4-directionally) to a rotten orange becomes rotten.
 
 Return the minimum number of minutes that must elapse until no cell has a fresh orange.  If this is impossible, return -1 instead.
- */
+*/
 public class RottingOranges {
 
     /**
--- src/main/java/com/flang/dfs/NumberOfIslands.java
+++ src/main/java/com/flang/dfs/NumberOfIslands.java
@@ -1,9 +1,56 @@
 package com.flang.dfs;
 
+import java.util.ArrayDeque;
+
+/**
+ * LC 200
+ */
 public class NumberOfIslands {
 
     final static int[][] dirs = {{-1,0},{1,0},{0,-1},{0,1}};
 
+    /**
+     * BFS
+     */
+    public int numIslandsBFS(char[][] grid) {
+        if (grid == null || grid.length == 0 || grid[0].length == 0) {
+            return 0;
+        }
+        int numOfIslands = 0;
+        final int rows = grid.length;
+        final int cols = grid[0].length;
+
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                if (grid[i][j] == '1') {
+                    numOfIslands++;
+                    bfs(grid, i, j);
+                }
+            }
+        }
+        return numOfIslands;
+    }
+
+    private void bfs(char[][] grid, int x, int y) {
+        ArrayDeque<int[]> queue = new ArrayDeque<>();
+        queue.offer(new int[]{x, y});
+        while(!queue.isEmpty()) {
+            int[] cur = queue.poll();
+            for (int[] dir : dirs) {
+                int nX = cur[0] + dir[0];
+                int nY = cur[1] + dir[1];
+                if (nX < 0 || nX >= grid.length || nY < 0 || nY >= grid[0].length) continue;
+                if (grid[nX][nY] == '1') {
+                    grid[nX][nY] = '0';
+                    queue.push(new int[]{nX, nY});
+                }
+            }
+        }
+    }
+
+    /**
+     * DFS
+     */
     public int numIslands(char[][] grid) {
         if (grid == null || grid.length == 0 || grid[0].length == 0) {
             return 0;
@@ -30,9 +77,9 @@ public class NumberOfIslands {
         // recursive rule
         grid[x][y] = '0';
         for (int[] dir : dirs) {
-            int neiX = dir[0] + x;
-            int neiY = dir[1] + y;
-            dfs(grid, neiX, neiY, rows, cols);
+            int nextX = dir[0] + x;
+            int nextY = dir[1] + y;
+            dfs(grid, nextX, nextY, rows, cols);
         }
     }
 
@@ -44,7 +91,8 @@ public class NumberOfIslands {
         System.out.println("row len: " + grids.length);
         System.out.println("col len: " + grids[0].length);
         NumberOfIslands numberOfIslands = new NumberOfIslands();
-        int count = numberOfIslands.numIslands(grids);
+        //int count = numberOfIslands.numIslands(grids);
+        int count = numberOfIslands.numIslandsBFS(grids);
         System.out.println(count);
     }
 }
