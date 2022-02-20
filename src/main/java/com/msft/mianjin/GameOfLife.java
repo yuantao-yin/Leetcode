b87e6ddff0eeefcca2eceed4914f004656e46a1c
Yuantao <yin0823@gmail.com>
2022-02-19 21:21:32 -0500

    update 2d array practice

--- src/main/java/com/msft/mianjin/GameOfLife.java
+++ src/main/java/com/msft/mianjin/GameOfLife.java
@@ -2,20 +2,16 @@ package com.msft.mianjin;
 
 public class GameOfLife {
 
+    static final int[][] dirs = new int[][] {
+            {-1, -1}, {-1, 0}, {-1, 1}, {0, -1},
+            {0, 1}, {1, -1}, {1, 0}, {1, 1}
+    };
+
     public void gameOfLife(int[][] board) {
         int r = board.length;
         int c = board[0].length;
         int[][] rst = new int[r][c];
-        int[][] dirs = new int[][] {
-                {-1, -1},
-                {-1, 0},
-                {-1, 1},
-                {0, -1},
-                {0, 1},
-                {1, -1},
-                {1, 0},
-                {1, 1},
-        };
+
         for (int i = 0; i < r; i++) {
             for (int j = 0; j < c; j++) {
                 int liveCounts = 0;
@@ -38,6 +34,36 @@ public class GameOfLife {
         for (int i = 0; i < r; i++) {
             for (int j = 0; j < c; j++) {
                 board[i][j] = rst[i][j];
+            }
+        }
+    }
+
+    public void gameOfLifeInPlace(int[][] board) {
+        int r = board.length;
+        int c = board[0].length;
+
+        for (int i = 0; i < r; i++) {
+            for (int j = 0; j < c; j++) {
+                int liveCounts = 0;
+                for (int[] dir : dirs) {
+                    int x = i + dir[0];
+                    int y = j + dir[1];
+                    if (x >= 0 && x < r && y >= 0 && y < c && Math.abs(board[x][y]) == 1) {
+                        liveCounts++;
+                    }
+                }
+
+                if (board[i][j] == 0 && liveCounts == 3) {
+                    board[i][j] = 2;
+                } else if (board[i][j] == 1 && (liveCounts < 2 || liveCounts > 3)) {
+                    board[i][j] = -1;
+                }
+
+            }
+        }
+        for (int i = 0; i < r; i++) {
+            for (int j = 0; j < c; j++) {
+                board[i][j] = board[i][j] > 0 ? 1 : 0;
             }
         }
     }
